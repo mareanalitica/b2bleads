b820786018200a49a412ff6f5d1a1c5ada688d81
mareanalitica <mareanalitica@gmail.com>
2023-09-08 03:11:53 -0300

    Retirando a dist do versionamento

--- dist/routes/resultsRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// execucaoRoutes.ts
-const express_1 = require("express");
-const resultsController_1 = require("../controllers/resultsController");
-const router = (0, express_1.Router)();
-router.get('/executions', resultsController_1.listAllResults);
-router.post('/execute', resultsController_1.search);
-exports.default = router;
